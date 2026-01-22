088b325bc24e6993a90cddadf8f57f7156f83065
Oliwia Walewska <58175490+woliwia@users.noreply.github.com>
2026-01-22 14:41:51 +0200

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
 
 export default defineConfig(({ mode }) => {
+    base: '/alveos-sm-report/',
     const env = loadEnv(mode, '.', '');
     return {
       server: {
